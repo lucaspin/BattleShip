b87d0586172e5b25bf7e626d4c22662ed0ea1cf7
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2016-03-23 22:15:30 -0300

    add method to display the grid

--- src/BattleShip/BattleShipGrid.java
+++ src/BattleShip/BattleShipGrid.java
@@ -8,8 +8,17 @@ import java.util.ArrayList;
 public class BattleShipGrid {
     private int verticalDimension;
     private int horizontalDimension;
+    private int occupiedCellsMaxNumber;
+    private int occupiedCellsCurrentNumber;
+    private boolean full = false;
     private GridCell[][] grid;
     private ArrayList<Ship> ships;
+
+    // Expose constants to limit the dimensions of the grid
+    public static final int MIN_VERTICAL_DIMENSION = 5;
+    public static final int MIN_HORIZONTAL_DIMENSION = 5;
+    public static final int MAX_VERTICAL_DIMENSION = 20;
+    public static final int MAX_HORIZONTAL_DIMENSION = 20;
 
     /**
      * @constructor
@@ -22,9 +31,55 @@ public class BattleShipGrid {
             this.verticalDimension = verticalDimension;
             this.horizontalDimension = horizontalDimension;
             this.initGrid();
+            this.setOccupiedCellsMaxNumber();
         } else {
             System.out.println("[BattleShip.BattleShipGrid.constructor] dimensions must be positive");
         }
+    }
+
+    /**
+     * Function that draws the grid in the terminal
+     */
+    public void displayGrid() {
+        for (int verticalCount = 0; verticalCount < this.verticalDimension; verticalCount++) {
+            for (int horizontalCount = 0; horizontalCount < this.horizontalDimension; horizontalCount++) {
+                System.out.print(this.grid[horizontalCount][verticalCount].getDisplayValue().getValue() + " ");
+            }
+            System.out.print("\n");
+        }
+
+    }
+
+    /**
+     * Set the max number of occupied cells, according to the grid's dimension
+     */
+    private void setOccupiedCellsMaxNumber() {
+        // TODO: accept a difficulty parameter, instead of setting 0.6 directly
+        this.occupiedCellsMaxNumber = Math.round((float) 0.6 * this.verticalDimension * this.horizontalDimension);
+    }
+
+    /**
+     * Set a grid's state to full
+     * @param full {boolean}
+     */
+    private void setFull(boolean full) {
+        this.full = full;
+    }
+
+    /**
+     * Check whether the grid is full
+     * @return full {boolean}
+     */
+    public boolean isFull() {
+        return this.full;
+    }
+
+    /**
+     * Get the number of occupied cells in the grid
+     * @return occupiedCellsCurrentNumber {int}
+     */
+    public int getOccupiedCellCurrentNumber() {
+        return this.occupiedCellsCurrentNumber;
     }
 
     /**
@@ -55,13 +110,15 @@ public class BattleShipGrid {
      * Initialize the BattleShipGrid
      */
     public void initGrid() {
-
         int horizontalDimension = this.getHorizontalDimension();
         int verticalDimension = this.getVerticalDimension();
 
-        // Create the grid and populate with GridCell
-        for (int horizontalIndex = 0; horizontalIndex <= horizontalDimension; horizontalIndex++) {
-            for(int verticalIndex = 0; verticalIndex <= verticalDimension; verticalIndex++) {
+        // Create grid
+        this.grid = new GridCell[horizontalDimension][verticalDimension];
+
+        // Populate it with GridCells
+        for (int verticalIndex = 0; verticalIndex < verticalDimension; verticalIndex++) {
+            for(int horizontalIndex = 0; horizontalIndex < horizontalDimension; horizontalIndex++) {
                 GridCell gridCell = new GridCell(horizontalIndex, verticalIndex);
                 this.grid[horizontalIndex][verticalIndex] = gridCell;
             }
@@ -119,28 +176,15 @@ public class BattleShipGrid {
      * @param ship the ship to add
      */
     public void addShip(Ship ship) {
-        if (this.isValidShipStartPosition(ship)) {
+        if (!this.isFull() && this.isValidShipStartPosition(ship)) {
             this.setShipParts(ship);
-            this.addShipToGrid(ship);
+            this.ships.add(ship);
+
+            // Update the number of occupied cells
+            this.occupiedCellsCurrentNumber += ship.getDimension();
         } else {
             System.out.println("[BattleShip.BattleShipGrid.addShip] the startPosition of the ship isn't part of the grid");
         }
-    }
-
-    /**
-     * Add a ship to the grid
-     * @param ship the ship to add
-     */
-    private void addShipToGrid(Ship ship) {
-        this.ships.add(ship);
-    }
-
-    /**
-     * Remove a ship from the grid
-     * @param ship the ship to remove
-     */
-    private void removeShipFromGrid(Ship ship) {
-        this.ships.remove(ship);
     }
 
     /**
@@ -186,4 +230,5 @@ public class BattleShipGrid {
         }
 
     }
+
 }
