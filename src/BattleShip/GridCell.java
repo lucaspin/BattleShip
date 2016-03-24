939e8693e216bb8dda750b6abc432a77fd80f4a2
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2016-03-23 21:46:08 -0300

    Add displayValue and alreadyGuessed properties to GridCell

--- src/BattleShip/GridCell.java
+++ src/BattleShip/GridCell.java
@@ -6,9 +6,11 @@ package BattleShip;
 public class GridCell extends Cell {
 
     private boolean empty = false;
+    private boolean alreadyGuessed = false;
+    private GridCellDisplayValue displayValue = GridCellDisplayValue.NO_GUESS;
 
     /**
-     * @constructor
+     * Constructor of the class
      * @param x the x coordinate of the gridCell
      * @param y the y coordinate of the gridCell
      */
@@ -18,19 +20,50 @@ public class GridCell extends Cell {
 
     /**
      * Check whether a GridCell is empty or not
-     * @return empty the state of the cell
+     * @return empty {boolean}
      */
     public boolean isEmpty() {
         return this.empty;
     }
 
     /**
-     * Set the state of the GridCell
-     * @param empty the new state of the GridCell
+     * Set if the GridCell is empty
+     * @param empty {boolean}
      */
     public void setEmpty(boolean empty) {
         this.empty = empty;
     }
 
+    /**
+     * Set if this GridCell has already been guessed
+     * @param alreadyGuessed {boolean}
+     */
+    public void setAlreadyGuessed(boolean alreadyGuessed) {
+        this.alreadyGuessed = alreadyGuessed;
+    }
+
+    /**
+     * Check whether a this GridCell has been already guessed
+     * @return alreadyGuessed {boolean}
+     */
+    public boolean isAlreadyGuessed() {
+        return this.alreadyGuessed;
+    }
+
+    /**
+     * Set the new display value of the GridCell
+     * @param displayValue {String}
+     */
+    public void setDisplayValue(GridCellDisplayValue displayValue) {
+       this.displayValue = displayValue;
+    }
+
+    /**
+     * Get the display value of the cell
+     * @return displayValue {String}
+     */
+    public GridCellDisplayValue getDisplayValue() {
+        return this.displayValue;
+    }
 }
 
---
+++ src/BattleShip/GridCellDisplayValue.java
@@ -0,0 +1,10 @@
+package BattleShip;
+
+/**
+ * @author lucaspinheiro
+ */
+public enum GridCellDisplayValue {
+    NO_GUESS,
+    GUESS_EMPTY,
+    GUESS_NON_EMPTY
+}
